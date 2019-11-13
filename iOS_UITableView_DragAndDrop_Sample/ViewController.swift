1c86c219642b6b7fdbaec83f01c8eebcbb6af2f1
RyoYamada <newfield1001@gmail.com>
2019-11-13 19:06:58 +0900

    Add model

--- iOS_UITableView_DragAndDrop_Sample/ViewController.swift
+++ iOS_UITableView_DragAndDrop_Sample/ViewController.swift
@@ -11,8 +11,21 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
 
+    let resource = TableResourceModel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        tableView.dataSource = self
     }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return resource.prefectureNames.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
